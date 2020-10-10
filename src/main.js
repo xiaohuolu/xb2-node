1b968135b2d701219f890606e1db9b061aec55a9
cxn <593900745@qq.com>
2020-10-10 17:12:48 +0800

    交换数据学习

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ const express = require('express')  //导入express
 const app = express()     // 制造出一个应用交给app
 const port = 3000    //端口
 
+/* 
+   使用JSON中间件
+*/
+app.use(express.json())
 
 //创建服务
 app.listen(port,()=>{
@@ -44,4 +48,26 @@ app.get('/posts/:postId',(request,response)=>{
 
    //做出响应
    response.send(posts[0])
+})
+
+/* 
+  创建内容
+*/
+app.post('/posts',(request,response)=>{
+    //获取请求里的数据
+    const {content} = request.body
+
+    //设置状态码
+    response.status(201)
+     
+    //输出请求头部数据
+    console.log(request.headers['sing-alone'])
+
+    //设置响应头部数据
+    response.set('Sing-Alone','How I wonder what you are!')
+
+    //做出响应
+    response.send({
+        'message':`成功创建了内容:${content}`
+    })
 })
