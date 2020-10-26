fd8ca04e0ab18c030a108e306d9d4a349b576918
cxn <593900745@qq.com>
2020-10-26 17:26:24 +0800

    创建用户模块

--- dist/app/database/mysql.js
+++ dist/app/database/mysql.js
@@ -7,7 +7,7 @@ const mysql2_1 = __importDefault(require("mysql2"));
 const app_config_1 = require("../../app/app.config");
 exports.connection = mysql2_1.default.createConnection({
     host: app_config_1.MYSQL_HOST,
-    port: parseInt(app_config_1.MYSQL_POST, 10),
+    port: parseInt(app_config_1.MYSQL_PORT, 10),
     user: app_config_1.MYSQL_USER,
     password: app_config_1.MYSQL_PASSWORD,
     database: app_config_1.MYSQL_DATABASE
