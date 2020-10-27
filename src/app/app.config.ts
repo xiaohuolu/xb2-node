7760babd953c66d5fcd20aaa0480c2de127e4c8f
cxn <593900745@qq.com>
2020-10-27 14:36:49 +0800

    准备密钥与公钥并配置

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config(); //会默认载入根目录下的env文件
 
@@ -12,9 +12,17 @@ export const { APP_PORT } = process.env;
  * 数据仓库配置
  */
 export const {
-    MYSQL_HOST,
-    MYSQL_PORT,
-    MYSQL_USER,
-    MYSQL_PASSWORD,
-    MYSQL_DATABASE
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
 } = process.env;
+
+/**
+ * 密钥配置
+ */
+export let { PRIVATE_KEY, PUBLIC_KEY } = process.env;
+//将base64处理的密钥还原成之前的
+PRIVATE_KEY = Buffer.from(PRIVATE_KEY, "base64").toString();
+PUBLIC_KEY = Buffer.from(PUBLIC_KEY, "base64").toString();
