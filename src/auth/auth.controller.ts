ce9bf84015b01d72b140ae93a219974c8d744330
cxn <593900745@qq.com>
2020-10-27 15:30:26 +0800

    签发令牌

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,28 @@
-import {Request,Response,NextFunction} from 'express';
+import { Request, Response, NextFunction } from "express";
+import { signToken } from "./auth.service";
 
 /**
  * 用户登录
  */
 export const login = async (
-    request:Request,
-    response:Response,
-    next:NextFunction
-)=>{
-    // 准备数据
-    const {name,password} = request.body;
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // 准备数据
+  const {
+    user: { id, name },
+  } = request.body;
+
+  const payload = { id, name };
+
+  try {
+    //签发令牌
+    const token = signToken({ payload });
 
     //做出响应
-    response.send({message:`欢迎回来，${name}`});
-}
+    response.send({ id, name, token });
+  } catch (error) {
+    next(error);
+  }
+};
