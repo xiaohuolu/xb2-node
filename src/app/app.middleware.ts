7ed7745d484f8bcb6a7256e244b55286371b28ff
cxn <593900745@qq.com>
2020-10-27 11:29:52 +0800

    验证用户登录数据

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 
 /**
  * 输出请求地址
@@ -6,7 +6,7 @@ import { Request, Response, NextFunction } from 'express';
 export const requestUrl = (
   request: Request,
   response: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
   console.log(request.url);
   next(); //执行以下next，这样这个请求才会继续被其他的中间件和接口处理器去处理,如果不执行的话会一直卡在这里
@@ -19,10 +19,10 @@ export const defaultErrorHandler = (
   error: any,
   request: Request,
   response: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
-  if(error.message){
-    console.log(error.message)
+  if (error.message) {
+    console.log(error.message);
   }
   let statusCode: number, message: string;
 
@@ -30,21 +30,25 @@ export const defaultErrorHandler = (
    * 处理异常
    */
   switch (error.message) {
-    case 'NAME_IS_REQUIRED':
+    case "NAME_IS_REQUIRED":
       statusCode = 400;
-      message = '请提供用户名';
+      message = "请提供用户名";
       break;
-      case 'PASSWORD_IS_REQUIRED':
-        statusCode = 400;
-        message = '请提供用户密码';
-        break;
-        case 'USER_ALREADY_EXIST':
-          statusCode = 409;
-          message = '用户名已被占用';
-          break;
+    case "PASSWORD_IS_REQUIRED":
+      statusCode = 400;
+      message = "请提供用户密码";
+      break;
+    case "USER_ALREADY_EXIST":
+      statusCode = 409;
+      message = "用户名已被占用";
+      break;
+    case "USER_DOES_NOT_EXIST":
+      statusCode = 400;
+      message = "用户不存在";
+      break;
     default:
       statusCode = 500;
-      message = '服务暂时出了点问题~';
+      message = "服务暂时出了点问题~";
       break;
   }
   response.status(statusCode).send({ message });
--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,2 +1,27 @@
 import {Request,Response,NextFunction} from 'express';
 import * as userService from '../user/user.service';
+
+/**
+ * 验证用户登录数据
+ */
+export const validateLoginData = async(
+    request:Request,
+    response:Response,
+    next:NextFunction
+)=>{
+   console.log('验证用户登录数据');
+
+   //准备数据
+   const {name,password} = request.body;
+
+   //验证必填数据
+   if(!name) return next(new Error('NAME_IS_REQUIRED'));
+   if(!password) return next(new Error('PASSWORD_IS_REQUIRED'));
+
+   //验证用户名
+   const user = await userService.getUserByName(name);
+   if(!user) return next(new Error("USER_DOES_NOT_EXIST"));
+
+   //下一步
+   next();
+}
