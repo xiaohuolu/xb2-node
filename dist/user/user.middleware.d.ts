f5f246de468bf989dfd85292086c06c235e3b52f
cxn <593900745@qq.com>
2020-10-27 10:32:56 +0800

    删除dist

--- dist/user/user.middleware.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const validateUserData: (request: Request<import("express-serve-static-core").ParamsDictionary>, response: Response<any>, next: NextFunction) => Promise<void>;
-export declare const hashPassword: (request: Request<import("express-serve-static-core").ParamsDictionary>, response: Response<any>, next: NextFunction) => Promise<void>;
