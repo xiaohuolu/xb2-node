1da51803a6ef4651ece6c3743e360c7d9e99a86c
cxn <593900745@qq.com>
2020-10-27 10:11:54 +0800

    删除dist

--- dist/app/app.config.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const APP_PORT: string;
-export declare const MYSQL_HOST: string, MYSQL_PORT: string, MYSQL_USER: string, MYSQL_PASSWORD: string, MYSQL_DATABASE: string;
--- dist/post/post.model.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class PostModel {
-    id?: number;
-    title?: string;
-    content?: string;
-}
