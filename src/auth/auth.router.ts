1b1808d6ad60f9df7dee65dc8b2322ee5bf7a690
cxn <593900745@qq.com>
2020-10-27 10:58:00 +0800

    创建身份验证模块

---
+++ src/auth/auth.router.ts
@@ -0,0 +1,12 @@
+import express, { request } from 'express';
+import * as authController from './auth.controller';
+
+const router = express.Router();
+
+
+
+
+/**
+ * 导出路由
+ */
+export default router;
