7a8bb3b6d0cecf1bc8a4670371df82c6fe4d5739
cxn <593900745@qq.com>
2020-10-27 09:13:02 +0800

    注册时检查用户名是否存在

--- dist/user/user.middleware.js
+++ dist/user/user.middleware.js
@@ -1,5 +1,13 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const userService = __importStar(require("./user.service"));
 exports.validateUserData = async (request, response, next) => {
     console.log('验证用户数据');
     const { name, password } = request.body;
@@ -7,6 +15,9 @@ exports.validateUserData = async (request, response, next) => {
         return next(new Error('NAME_IS_REQUIRED'));
     if (!password)
         return next(new Error('PASSWORD_IS_REQUIRED'));
+    const user = await userService.getUserByName(name);
+    if (user)
+        return next(new Error('USER_ALREADY_EXIST'));
     next();
 };
 //# sourceMappingURL=user.middleware.js.map
