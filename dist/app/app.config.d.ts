802bdb678b5b7caed278e47b313cafd22c784d30
cxn <593900745@qq.com>
2020-10-27 10:10:44 +0800

    删除dist

--- dist/app/app.config.d.ts
+++ dist/app/app.config.d.ts
@@ -1,2 +1,2 @@
 export declare const APP_PORT: string;
-export declare const MYSQL_HOST: string, MYSQL_POST: string, MYSQL_USER: string, MYSQL_PASSWORD: string, MYSQL_DATABASE: string;
+export declare const MYSQL_HOST: string, MYSQL_PORT: string, MYSQL_USER: string, MYSQL_PASSWORD: string, MYSQL_DATABASE: string;
