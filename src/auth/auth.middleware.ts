272bc303626ea87e0ebbe2594f7ccafe09cd2429
cxn <593900745@qq.com>
2020-10-27 11:50:29 +0800

    验证用户密码是否匹配

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,27 +1,32 @@
-import {Request,Response,NextFunction} from 'express';
-import * as userService from '../user/user.service';
+import { Request, Response, NextFunction } from "express";
+import * as userService from "../user/user.service";
+import bcrypt from "bcrypt";
 
 /**
  * 验证用户登录数据
  */
-export const validateLoginData = async(
-    request:Request,
-    response:Response,
-    next:NextFunction
-)=>{
-   console.log('验证用户登录数据');
+export const validateLoginData = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  console.log("验证用户登录数据");
 
-   //准备数据
-   const {name,password} = request.body;
+  //准备数据
+  const { name, password } = request.body;
 
-   //验证必填数据
-   if(!name) return next(new Error('NAME_IS_REQUIRED'));
-   if(!password) return next(new Error('PASSWORD_IS_REQUIRED'));
+  //验证必填数据
+  if (!name) return next(new Error("NAME_IS_REQUIRED"));
+  if (!password) return next(new Error("PASSWORD_IS_REQUIRED"));
 
-   //验证用户名
-   const user = await userService.getUserByName(name);
-   if(!user) return next(new Error("USER_DOES_NOT_EXIST"));
+  //验证用户名
+  const user = await userService.getUserByName(name, { password: true });
+  if (!user) return next(new Error("USER_DOES_NOT_EXIST"));
 
-   //下一步
-   next();
-}
+  //验证用户密码
+  const matched = await bcrypt.compare(password, user.password);
+  if (!matched) return next(new Error("PASSWORD_DOES_NOT_MATCH"));
+
+  //下一步
+  next();
+};
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,37 +1,49 @@
-import {connection} from '../app/database/mysql';
-import {UserModel} from './user.model';
+import { connection } from "../app/database/mysql";
+import { UserModel } from "./user.model";
 
 /**
  * 创建用户
  */
-export const createUser = async (user:UserModel)=>{
-   //准备查询
-   const statement=`
+export const createUser = async (user: UserModel) => {
+  //准备查询
+  const statement = `
     INSERT INTO user
     SET ? 
   `;
 
   //执行查询
-  const [data] = await connection.promise().query(statement,user);
+  const [data] = await connection.promise().query(statement, user);
 
   //提供数据
   return data;
-}
+};
 
 /**
  * 按用户名查找用户
  */
-export const getUserByName = async (name:string)=>{
-    //准备查询
-    const statement = `
-      SELECT id, name
+interface GetUserOptions {
+  password?: boolean;
+}
+export const getUserByName = async (
+  name: string,
+  options: GetUserOptions = {}
+) => {
+  //准备选项
+  const { password } = options;
+
+  //准备查询
+  const statement = `
+      SELECT 
+       id,
+       name
+       ${password ? ",password" : ""}
       FROM user
       WHERE name = ?
-    `
+    `;
 
-    //执行查询
-    const [data] = await connection.promise().query(statement,name);
+  //执行查询
+  const [data] = await connection.promise().query(statement, name);
 
-    //提供数据
-    return data[0];
-}
+  //提供数据
+  return data[0];
+};
