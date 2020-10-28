87a1f95b7bedd4ee36569af2df8888e6099100a2
cxn <593900745@qq.com>
2020-10-28 16:52:46 +0800

    定义上传文件接口

--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import { authGuard } from "src/auth/auth.middleware";
 import * as fileController from "./file.controller";
+import { fileInterceptor } from "./file.middleware";
 
 const router = express.Router();
+
+/**
+ * 上传文件
+ */
+router.post("/files", authGuard, fileInterceptor, fileController.store);
 
 /**
  * 导出路由
