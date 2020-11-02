5ead5b3ec3ccb233613433a2f819f4acf77f8cb6
cxn <593900745@qq.com>
2020-11-02 17:37:07 +0800

    定义删除评论接口

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -4,6 +4,7 @@ import {
   createComment,
   isReplyComment,
   updateComment,
+  deleteComment,
 } from "./comment.service";
 
 /**
@@ -94,6 +95,28 @@ export const update = async (
   try {
     // 修改评论
     const data = await updateComment(comment);
+    // 做出响应
+    response.send(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * 删除评论
+ */
+export const destroy = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // 准备数据
+  const { commentId } = request.params;
+
+  try {
+    // 删除评论
+    const data = await deleteComment(parseInt(commentId, 10));
+
     // 做出响应
     response.send(data);
   } catch (error) {
