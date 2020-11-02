c7cdc81c03fb48e10cee59ea272cfec2a2ab6932
cxn <593900745@qq.com>
2020-11-02 10:59:31 +0800

    创建标签模块、定义存储标签数据的功能、定义按名称查找标签的功能、定义创建标签用的接口

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -62,6 +62,10 @@ export const defaultErrorHandler = (
       statusCode = 404;
       message = "文件不存在";
       break;
+    case "TAG_ALREADY_EXISTS":
+      statusCode = 400;
+      message = "标签已存在";
+      break;
     default:
       statusCode = 500;
       message = "服务暂时出了点问题~";
