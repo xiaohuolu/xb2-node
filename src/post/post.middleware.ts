26414e8a61a7416ae0cf7e46bce9064f6acab7c5
cxn <593900745@qq.com>
2020-11-03 16:30:34 +0800

    内容列表改造

---
+++ src/post/post.middleware.ts
@@ -0,0 +1,77 @@
+import { Request, Response, NextFunction } from "express";
+
+/**
+ *  排序方式
+ */
+export const sort = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // 获取客户端的排序方式
+  const { sort } = request.query;
+
+  // 排序用的sql
+  let sqlSort: string;
+
+  // 设置培训用的sql
+  switch (sort) {
+    case "earliest":
+      sqlSort = "post.id ASC";
+      break;
+    case "latest":
+      sqlSort = "post.id DESC";
+      break;
+    case "most_comments":
+      sqlSort = "totalComments DESC ,post.id DESC";
+      break;
+
+    default:
+      sqlSort = "post.id DESC";
+      break;
+  }
+
+  //   在请求中添加排序
+  request.sort = sqlSort;
+
+  //   下一步
+  next();
+};
+
+/**
+ *  过滤列表
+ */
+export const filter = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // 解构查询符
+  const { tag, user, action } = request.query;
+
+  // 设置默认的过滤
+  request.filter = {
+    name: "default",
+    sql: "post.id IS NOT NULL",
+  };
+
+  //   按标签名过滤
+  if (tag && !user && !action) {
+    request.filter = {
+      name: "tagName",
+      sql: "tag.name = ?",
+      param: tag,
+    };
+  }
+
+  //   过滤出用户发布的内容
+  if (user && action == "published" && !tag) {
+    request.filter = {
+      name: "userPublished",
+      sql: "user.id=?",
+      param: user,
+    };
+  }
+  //   下一步
+  next();
+};
--- types/express.d.ts
+++ types/express.d.ts
@@ -1,10 +1,13 @@
 import { TokenPayload } from "../src/auth/auto.interface";
+import { GetPostsOptionsFilter } from "../src/post/post.service";
 
 declare global {
   namespace Express {
     export interface Request {
       user: TokenPayload;
       fileMetaData: { width?: number; height?: number; metadata?: {} };
+      sort: string;
+      filter: GetPostsOptionsFilter;
     }
   }
 }
