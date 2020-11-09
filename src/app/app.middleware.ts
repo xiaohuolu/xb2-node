97d8d8c8af61a7322dfe919a3a5f29d6f4ea705e
cxn <593900745@qq.com>
2020-11-09 15:26:49 +0800

    定义按ID调取内容的功能

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -78,6 +78,10 @@ export const defaultErrorHandler = (
       statusCode = 400;
       message = "不能上传此类型文件";
       break;
+    case "NOT_FOUND ":
+      statusCode = 404;
+      message = "没找到~~";
+      break;
     default:
       statusCode = 500;
       message = "服务暂时出了点问题~";
