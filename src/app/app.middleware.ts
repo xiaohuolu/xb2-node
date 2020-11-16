d9f907e6edb509fdcfd14dda0bc9f5cd073ed727
cxn <593900745@qq.com>
2020-11-16 17:28:41 +0800

    创建验证更新用户数据中间件

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -82,6 +82,14 @@ export const defaultErrorHandler = (
       statusCode = 404;
       message = "没找到~~";
       break;
+    case "USER_NOT_FOUND":
+      statusCode = 404;
+      message = "没找到这个用户~~";
+      break;
+    case "PASSWORD_IS_THE_SAME":
+      statusCode = 400;
+      message = "要修改的密码不能跟原密码一样~~";
+      break;
     default:
       statusCode = 500;
       message = "服务暂时出了点问题~";
--- src/user/user.middleware.ts
+++ src/user/user.middleware.ts
@@ -1,46 +1,103 @@
-import {Request,Response,NextFunction, request, response} from 'express';
-import bcrypt from 'bcrypt';
-import * as userService from './user.service';
+import { Request, Response, NextFunction, request, response } from "express";
+import bcrypt from "bcrypt";
+import _ from "lodash";
+import * as userService from "./user.service";
 
 /**
  * 验证用户数据
  */
 export const validateUserData = async (
-    request:Request,
-    response:Response,
-    next:NextFunction
-)=>{
-   console.log('验证用户数据');
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  console.log("验证用户数据");
 
-   //准备数据
-   const {name,password} = request.body;
+  //准备数据
+  const { name, password } = request.body;
 
-   //验证必填数据
-   if(!name) return next(new Error('NAME_IS_REQUIRED'));
-   if(!password) return next(new Error('PASSWORD_IS_REQUIRED'));
+  //验证必填数据
+  if (!name) return next(new Error("NAME_IS_REQUIRED"));
+  if (!password) return next(new Error("PASSWORD_IS_REQUIRED"));
 
-   //验证用户名
-   const user = await userService.getUserByName(name);
-   if(user) return next(new Error('USER_ALREADY_EXIST'));
+  //验证用户名
+  const user = await userService.getUserByName(name);
+  if (user) return next(new Error("USER_ALREADY_EXIST"));
 
-   //下一步 
-    next();
-}
+  //下一步
+  next();
+};
 
 /**
  * HASH 密码
  */
 export const hashPassword = async (
-    request:Request,
-    response:Response,
-    next:NextFunction
-)=>{
-   // 准备数据
-   const {password} = request.body;
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // 准备数据
+  const { password } = request.body;
 
-   //HASH密码
-   request.body.password = await bcrypt.hash(password,10);
+  //HASH密码
+  request.body.password = await bcrypt.hash(password, 10);
 
-   //下一步
-   next();
-}
+  //下一步
+  next();
+};
+
+/**
+ * 验证更新用户数据
+ */
+export const validateUpdateUserData = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  // 准备数据
+  const { validate, update } = request.body;
+  //   当前用户
+  const { id: userId } = request.user;
+
+  try {
+    //   检查用户是否提供了当前密码
+    if (!_.has(validate, "password")) {
+      return next(new Error("PASSWORD_IS_REQUIRE"));
+    }
+
+    // 调取用户数据
+    const user = await userService.getUserById(userId, { password: true });
+
+    // 验证用户密码是否匹配
+    const matched = await bcrypt.compare(validate.password, user.password);
+
+    if (!matched) {
+      return next(new Error("PASSWORD_DOES_NOT_MATCH"));
+    }
+    // 检查用户名是否被占用
+    if (update.name) {
+      const user = await userService.getUserByName(update.name);
+
+      if (user) {
+        return next(new Error("USER_ALREADY_EXIST"));
+      }
+    }
+
+    // 处理用户更新密码
+    if (update.password) {
+      const matched = await bcrypt.compare(update.password, user.password);
+
+      if (matched) {
+        return next(new Error("PASSWORD_IS_THE_SAME"));
+      }
+
+      //   hash 用户更新密码
+      request.body.update.password = await bcrypt.hash(update.password, 10);
+    }
+  } catch (error) {
+    return next(error);
+  }
+
+  //   下一步
+  next();
+};
