5112791bc82495fa547455b67290dd99be7c4f35
cxn <593900745@qq.com>
2020-11-16 16:37:37 +0800

    统计评论数量

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -6,6 +6,7 @@ import {
   updateComment,
   deleteComment,
   getComments,
+  getCommentsTotalCount,
 } from "./comment.service";
 
 /**
@@ -133,6 +134,16 @@ export const index = async (
   response: Response,
   next: NextFunction
 ) => {
+  // 统计评论数量
+  try {
+    const totalCount = await getCommentsTotalCount({ filter: request.filter });
+
+    // 设置响应头部
+    response.header("X-Total-Count", totalCount);
+  } catch (error) {
+    next(error);
+  }
+
   // 获取评论列表
   try {
     const comments = await getComments({
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -80,12 +80,12 @@ export const deleteComment = async (commentId: number) => {
  * 获取评论列表
  */
 
-interface GetCommentOptions {
+interface GetCommentsOptions {
   filter?: GetPostsOptionsFilter;
   pagination?: GetPostsOptionsPagination;
 }
 
-export const getComments = async (options: GetCommentOptions) => {
+export const getComments = async (options: GetCommentsOptions) => {
   // 结构选择
   const {
     filter,
@@ -127,4 +127,39 @@ export const getComments = async (options: GetCommentOptions) => {
 
   // 提供数据
   return data;
+};
+
+/**
+ * 统计评论数量
+ */
+export const getCommentsTotalCount = async (options: GetCommentsOptions) => {
+  // 结构选项
+  const { filter } = options;
+
+  // sql参数
+  let params: Array<any> = [];
+
+  // 设置sql参数
+  if (filter.param) {
+    params = [filter.param, ...params];
+  }
+
+  // 准备查询
+  const statement = `
+     SELECT
+      COUNT(
+        DISTINCT comment.id
+      ) as total
+     FROM
+      comment
+    ${sqlFragment.leftJoinUser}
+    ${sqlFragment.leftJoinPost}
+     WHERE
+    ${filter.sql}
+    `;
+  // 执行查询
+  const [data] = await connection.promise().query(statement, params);
+
+  // 提供结果
+  return data[0].total;
 };
